1213acd60100012531286a1bd928558d1c84d417
samikshaz-angularminds <samiksha@angularminds.in>
2025-04-09 15:07:51 +0530

    did something

--- backendSocially/utils/asyncErrorHandle.utils.js
+++ backendSocially/utils/asyncErrorHandle.utils.js
@@ -1,7 +1,18 @@
-const asyncErrorHandler = (requestHandler) =>{
-    return (req,res,next) => {
-        Promise.resolve(requestHandler(req,res,next).catch(err => next(err)) );
-    }
-};
+const asyncErrorHandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => {
+      console.log("error in error handler: ",err);
+      
+      return next(err)});
+  }
+    
+    
+    
+//     requestHandler) =>{
+//     return (req,res,next) => {
+//         Promise.resolve(requestHandler(req,res,next).catch(err => next(err)) );
+//     }
+// };
+
+
 
 export {asyncErrorHandler};
